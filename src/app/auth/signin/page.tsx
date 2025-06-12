95207c4cb7a7ab618cb1a01e36dc8de63d3c5e4c
LeeWonbae <judiking1@gmail.com>
2025-06-13 01:52:45 +0900

    로그인 후 메인페이지(저장소)로 이동

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import { FaGoogle } from 'react-icons/fa';
 
 async function getLoginUrl() {
-  const redirectUrl = 'https://unimages.com/storage';
+  const redirectUrl = 'https://unimages.com';
   return `https://api.unimages.com/auth/login?redirectUrl=${encodeURIComponent(redirectUrl)}`;
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,23 @@ import { redirect } from 'next/navigation';
 import StorageClient from './components/storage/StorageClient';
 import Link from 'next/link';
 import { FaPlus } from 'react-icons/fa';
-
-// 동적 렌더링 강제
-export const dynamic = 'force-dynamic';
+import { headers } from 'next/headers';
 
 async function checkAuth() {
   try {
-    const response = await fetch('https://api.unimages.com/auth/check', {
-      credentials: 'include',
+    const headersList = await headers();
+    const cookie = headersList.get('cookie') || '';
+    const response = await fetch(`https://api.unimages.com/auth/check`, {
+      headers: {
+        'Cookie': cookie,
+      },
     });
-    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error('인증 상태 확인 실패');
+    }
+
+    const { data } = await response.json();
     return data.authenticated;
   } catch (error) {
     console.error('인증 상태 확인 실패:', error);
@@ -21,25 +28,30 @@ async function checkAuth() {
 }
 
 export default async function Home() {
-  const isAuthenticated = await checkAuth();
 
-  if (!isAuthenticated) {
+  try {
+    const isAuthenticated = await checkAuth();
+    if (!isAuthenticated) {
+      redirect('/about');
+    } else {
+      return (
+        <div className="container mx-auto px-4 py-8">
+          <div className="flex justify-between items-center mb-8">
+            <h1 className="text-3xl font-bold">내 스크린샷 저장소</h1>
+            <Link
+              href="/upload"
+              className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 flex items-center space-x-2 transition-colors"
+            >
+              <FaPlus />
+              <span>이미지 업로드</span>
+            </Link>
+          </div>
+          <StorageClient />
+        </div>
+      )
+    }
+  } catch (error) {
+    console.error('인증 상태 확인 실패:', error);
     redirect('/about');
   }
-
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <div className="flex justify-between items-center mb-8">
-        <h1 className="text-3xl font-bold">내 스크린샷 저장소</h1>
-        <Link
-          href="/upload"
-          className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 flex items-center space-x-2 transition-colors"
-        >
-          <FaPlus />
-          <span>이미지 업로드</span>
-        </Link>
-      </div>
-      <StorageClient />
-    </div>
-  );
 }
