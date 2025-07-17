78237c45a50c08e584218f3d9f1ce15c47ec436c
wonbae <judiking1@naver.com>
2025-07-18 01:11:17 +0900

    목록 페이지에 현재 사용 용량 표시

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,18 @@ export default function Home() {
     <div className="container mx-auto px-4 py-8">
       <div className="flex justify-between items-center mb-8">
         <h1 className="text-3xl font-bold">내 스크린샷 저장소</h1>
-        <Link
-          href="/upload"
-          className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 flex items-center space-x-2 transition-colors"
-        >
-          <FaPlus />
-          <span>이미지 업로드</span>
-        </Link>
+        <div className='flex items-center gap-4'>
+          <div>
+            용량 : 100MB / 1GB
+          </div>
+          <Link
+            href="/upload"
+            className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 flex items-center space-x-2 transition-colors"
+          >
+            <FaPlus />
+            <span>이미지 업로드</span>
+          </Link>
+        </div>
       </div>
       <StorageClient />
     </div>
